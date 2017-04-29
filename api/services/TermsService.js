92ccc810022e65edc049387b17c471dbbcde2a0f
gladyslim <gladysl@seekasia.com>
2017-04-29 15:40:58 +0800

    updates

--- api/services/TermsService.js
+++ api/services/TermsService.js
@@ -5,19 +5,18 @@ const strip = require('striptags');
 module.exports = {
     createNewTerm: async function (termInfo) {
         return await Terms.create({
-            key : termInfo.name,
+            key : termInfo.key,
             description: termInfo.description,
             image: termInfo.image,
             source: termInfo.source,
-            user_name: termInfo.username,
-            user_role: termInfo.userrole,
+            user_name: termInfo.user_name,
+            user_role: termInfo.user_role,
         });
     },
 
-    upvoteTerm: async function (termId, termInfo) {
+    upvoteTerm: async function (termId) {
         try {
-            const term = await
-            Terms.findOne({ id: termId });
+            const term = await Terms.findOne({ id: termId });
             term.upvote_count++;
             return await term.save();
         } catch (error) {
@@ -26,14 +25,13 @@ module.exports = {
     },
 
     findTermInfo: function(text) {
-        let terms = require('./geology');
-        terms = terms.sort(function(a, b){
+        let terms = Terms.find().sort(function(a, b){
             // ASC  -> a.length - b.length
             // DESC -> b.length - a.length
             return strip(b.key).length - strip(a.key).length;
         });
 
-        for (let term of terms) {
+        for (let term of Object.entries(terms)) {
             let key = strip(term.key);
             let match = text.match(new RegExp(key , 'i'));
             if (match && ~match.index) {
