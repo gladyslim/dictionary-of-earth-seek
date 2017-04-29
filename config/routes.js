adb4d05b7657f50f769d5590fe68768b7d587417
hi <hi>
2017-04-29 13:09:21 +0800

    Added a page of list of entries

--- config/routes.js
+++ config/routes.js
@@ -2,5 +2,7 @@ module.exports.routes = {
   'post /term': 'TermsController.findTermInfo',
   'put /term/:id?': 'TermsController.updateTermInfo',
   'post /term/new': 'TermsController.createTermInfo',
-  'get /term/upvote/:id' : 'TermsController.upvote'
+  'get /term/upvote/:id' : 'TermsController.upvote',
+  'get /entry/new' : 'UserController.newterm',
+  'get /entries' : 'UserController.list'
 };
