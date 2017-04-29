456b80ffb60fa0aef10143323cc207326da9c58d
gladyslim <gladysl@seekasia.com>
2017-04-29 16:29:33 +0800

    updates

--- api/services/TermsService.js
+++ api/services/TermsService.js
@@ -4,14 +4,18 @@ const strip = require('striptags');
 
 module.exports = {
     createNewTerm: async function (termInfo) {
-        return await Terms.create({
-            key : termInfo.key,
-            description: termInfo.description,
-            image: termInfo.image,
-            source: termInfo.source,
-            user_name: termInfo.user_name,
-            user_role: termInfo.user_role,
-        });
+        try {
+            return await Terms.create({
+                key: termInfo.key,
+                description: termInfo.description,
+                image: termInfo.image,
+                source: termInfo.source,
+                user_name: termInfo.user_name,
+                user_role: termInfo.user_role,
+            });
+        } catch (err) {
+            return err;
+        }
     },
 
     upvoteTerm: async function (termId) {
@@ -19,24 +23,26 @@ module.exports = {
             const term = await Terms.findOne({ id: termId });
             term.upvote_count++;
             return await term.save();
-        } catch (error) {
-            return error;
+        } catch (err) {
+            return err;
         }
     },
 
-    findTermInfo: function(text) {
-        let terms = Terms.find().sort(function(a, b){
-            // ASC  -> a.length - b.length
-            // DESC -> b.length - a.length
-            return strip(b.key).length - strip(a.key).length;
-        });
-
-        for (let term of Object.entries(terms)) {
-            let key = strip(term.key);
-            let match = text.match(new RegExp(key , 'i'));
-            if (match && ~match.index) {
-                console.log("> " , key , "Index" , match ? match.index : 0);
+    findTermInfo: async function(text) {
+        try {
+            const terms = await Terms.find().sort({ key: 'DESC' });
+            let matchedKeys = [];
+            for (let term of terms) {
+                let key = strip(term.key);
+                let match = text.match(new RegExp(key, 'i'));
+                if (match && ~match.index) {
+                    matchedKeys.push(term);
+                }
             }
+
+            return matchedKeys;
+        } catch (err) {
+            return err;
         }
     }
 };
